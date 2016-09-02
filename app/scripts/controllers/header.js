4554432dbc818ddb44bb28ef5fe9fc971d79f352
maxence <roger666@hotmail.com>
2016-09-02 15:59:47 -0400

    first commit

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('HeaderCtrl', function () {
+   
+  });
